da54af9f41099a999fe247bde71a981b6bfa3c19
Sukhjinder Arora <sukhb1995@gmail.com>
2018-12-09 17:30:48 +0530

    finished styling the app

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,16 @@
 import '@babel/polyfill';
 
 import '../styles/main.scss';
+
+/**
+ * TODO(s)
+ * 1. Design the overall webpage in HTML and CSS
+ * 2. Required Elements: Input field, Submit button, Image
+ * 3. Develop the app using http://goqr.me/api/ Api
+ * 4. Use Axios lib for async network requests
+ * 
+ * FEATURES:
+ * 1. Enter the URL or text to generate the QR Code
+ * 2. Generate an image in the image container
+ * 3. Have an option to either download or print the image
+ */
